83a05178e684659fe1844487e4f43a727d7c74a1
nabhcmus <nabsubyt1@gmail.com>
2025-10-28 16:18:24 +0700

    feat: Setup frontend infrastructure and initial state management

---
+++ lib/ApolloProvider.tsx
@@ -0,0 +1,40 @@
+// lib/ApolloProvider.tsx
+"use client";
+
+import { ApolloLink, HttpLink } from "@apollo/client";
+import {
+  ApolloNextAppProvider,
+  NextSSRApolloClient,
+  NextSSRInMemoryCache,
+  SSRMultipartLink,
+} from "@apollo/experimental-nextjs-app-support/ssr";
+
+// Hàm makeClient này dùng để tạo client instance cho Provider
+function makeClient() {
+  const httpLink = new HttpLink({
+    // Bạn vẫn cần thay thế URL này bằng API thật của mình
+    uri: "https://your-graphql-api-endpoint.com/graphql",
+    fetchOptions: { cache: "no-store" },
+  });
+
+  return new NextSSRApolloClient({
+    cache: new NextSSRInMemoryCache(),
+    link:
+      typeof window === "undefined"
+        ? ApolloLink.from([
+            new SSRMultipartLink({
+              stripDefer: true,
+            }),
+            httpLink,
+          ])
+        : httpLink,
+  });
+}
+
+export function ApolloProvider({ children }: React.PropsWithChildren) {
+  return (
+    <ApolloNextAppProvider makeClient={makeClient}>
+      {children}
+    </ApolloNextAppProvider>
+  );
+}
